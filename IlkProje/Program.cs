37df9446da12d2351cac8d0d8697b5cf47ea3502
info@yazilimfikirleri.com <info@yazilimfikirleri.com>
2020-05-08 11:11:26 +0300

    proje oluşturuldu.

---
+++ IlkProje/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkProje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Merhaba Dünya");//Console.Write() metodu ekrana yazmak istediğimiz metinler için kullanırız.
+            Console.Read();//Console.Read() metodu console'un kapanmaması için kullanırız.
+
+            //www.yazilimfikirleri.com
+        }
+    }
+}
